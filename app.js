998ab2b10f022214d60af44e412d7d25d3ca3a64
dat208 <102476443+dat208@users.noreply.github.com>
2024-01-17 23:01:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const yourDate = new Date("2022-01-25T00:00:00"),
-music = ['ido', 'noinaycoanh', 'nguoiamphu'];
+music = ['ido', 'noinaycoanh', 'EM XINH'];
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
